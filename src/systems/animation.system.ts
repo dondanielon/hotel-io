d0f22fff6ddadac0d81722d831721682e0f8b66b
Daniel Valencia <becse.ca@gmail.com>
2025-04-12 20:35:26 -0600

    add animation system

---
+++ src/systems/animation.system.ts
@@ -0,0 +1,7 @@
+import { ECSYThreeSystem } from 'ecsy-three';
+
+export class AnimationSystem extends ECSYThreeSystem {
+  static queries = {};
+
+  execute(_delta: number, _time: number): void {}
+}
