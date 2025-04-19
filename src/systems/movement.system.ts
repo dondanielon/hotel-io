63278ef2506af4952c1618b8fa78aed41d3c6c02
Daniel Valencia <becse.ca@gmail.com>
2025-04-19 00:20:00 -0600

    remove animation logic from movement system

--- src/systems/movement.system.ts
+++ src/systems/movement.system.ts
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { MovementComponent } from '@root/components/movement.component';
-import { PlayerAnimationComponent } from '@root/components/player-animation.component';
 import { PlayerComponent } from '@root/components/player.component';
 import { ECSYThreeSystem } from 'ecsy-three';
 import { Vector3 } from 'three';
@@ -12,7 +11,7 @@ import { Constants } from '@root/constants';
 export class MovementSystem extends ECSYThreeSystem {
   static queries = {
     players: {
-      components: [PlayerComponent, MovementComponent, PlayerAnimationComponent],
+      components: [PlayerComponent, MovementComponent],
     },
   };
 
@@ -20,9 +19,6 @@ export class MovementSystem extends ECSYThreeSystem {
     this.queries.players.results.forEach((entity) => {
       const playerMesh = entity.getObject3D<THREE.Mesh>()!;
       const movementComponent = entity.getMutableComponent(MovementComponent)!;
-      const animationComponent = entity.getComponent(PlayerAnimationComponent)!;
-
-      animationComponent.mixer.update(delta);
 
       if (!movementComponent.isMoving || !movementComponent.targetPosition) return;
 
@@ -34,10 +30,8 @@ export class MovementSystem extends ECSYThreeSystem {
       const step = direction.multiplyScalar(distance);
 
       if (playerMesh.position.distanceTo(movementComponent.targetPosition) > distance) {
-        this.handleMovingState(animationComponent);
         playerMesh.position.add(step);
       } else {
-        this.handleStoppedState(animationComponent);
         playerMesh.position.copy(movementComponent.targetPosition);
         movementComponent.isMoving = false;
         movementComponent.targetPosition = null;
@@ -45,20 +39,6 @@ export class MovementSystem extends ECSYThreeSystem {
 
       playerMesh.rotation.y = this.calculateRotationY(playerMesh.rotation.y, direction, delta);
     });
-  }
-
-  private handleMovingState(animationComponent: PlayerAnimationComponent): void {
-    if (!animationComponent.walk.isRunning()) {
-      animationComponent.idle.fadeOut(Constants.PLAYER_ANIMATION_FADE_DURATION);
-      animationComponent.walk.reset().fadeIn(Constants.PLAYER_ANIMATION_FADE_DURATION).play();
-    }
-  }
-
-  private handleStoppedState(animationComponent: PlayerAnimationComponent): void {
-    if (animationComponent.walk.isRunning()) {
-      animationComponent.walk.fadeOut(Constants.PLAYER_ANIMATION_FADE_DURATION);
-      animationComponent.idle.reset().fadeIn(Constants.PLAYER_ANIMATION_FADE_DURATION).play();
-    }
   }
 
   private calculateRotationY(currentRotationY: number, direction: Vector3, delta: number): number {
