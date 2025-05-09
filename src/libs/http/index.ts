a46960bfffeaccd0e7f7f388f9618dda9885b82f
Daniel Valencia <becse.ca@gmail.com>
2025-05-08 20:59:16 -0600

    --testing with ngrok--

--- src/libs/http/index.ts
+++ src/libs/http/index.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API_URL,
+  // baseURL: import.meta.env.VITE_BASE_API_URL,
+  baseURL: "https://1ab1-2806-103e-18-2cfe-ecfd-b69c-7117-c197.ngrok-free.app:3000",
   timeout: 10_000,
 });
 
