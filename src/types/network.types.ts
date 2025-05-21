eaecb31a87916d5f13c72446320dd22e3257b7a9
Daniel Valencia <becse.ca@gmail.com>
2025-05-20 23:22:38 -0600

    fix network types

--- src/types/network.types.ts
+++ src/types/network.types.ts
@@ -1,31 +1,13 @@
-export interface NetworkPlayer {
-  username: string;
-  skin: string;
-  position: {
-    x: number;
-    y: number;
-    z: number;
-  };
-  targetPosition: {
-    x: number;
-    y: number;
-    z: number;
-  } | null;
-}
-
-export interface NetworkTerrain {
-  id: string;
-  name: string;
-  points: Array<{ x: number; y: number }>;
-}
+import { Player, Terrain } from "./game.types";
 
 export interface NetworkJoinGamePayload {
   gameId: string;
   host: string;
   name: string;
-  players: Record<string, NetworkPlayer>;
+  players: Record<string, Player>;
+  terrain: Terrain;
 }
 
 export interface NetworkGameState {
-  players: Record<string, NetworkPlayer>;
+  players: Record<string, Player>;
 }
