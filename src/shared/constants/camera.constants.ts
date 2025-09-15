3f90f2bfa798ef9fe2979520938188b623d6e447
Daniel Valencia <becse.ca@gmail.com>
2025-09-14 21:35:02 -0600

    adjust camera initial position

--- src/shared/constants/camera.constants.ts
+++ src/shared/constants/camera.constants.ts
@@ -4,6 +4,6 @@ export class CameraConstants {
   static readonly FOV = 40;
   static readonly NEAR_VIEW = 0.1;
   static readonly X_POSITION_ADD = 0;
-  static readonly Y_POSITION_ADD = 10;
-  static readonly Z_POSITION_ADD = 10;
+  static readonly Y_POSITION_ADD = 12;
+  static readonly Z_POSITION_ADD =10;
 }
