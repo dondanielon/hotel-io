cc23a6f8ac2961c68c518a2fe7219651c3c1eb3a
Daniel Valencia <becse.ca@gmail.com>
2025-10-16 20:33:31 -0600

    fix: camera fov

--- src/shared/constants/camera.constants.ts
+++ src/shared/constants/camera.constants.ts
@@ -1,7 +1,7 @@
 export class CameraConstants {
   static readonly ASPECT_RATIO = window.innerWidth / window.innerHeight;
   static readonly FAR_VIEW = 1000;
-  static readonly FOV = 20;
+  static readonly FOV = 40;
   static readonly NEAR_VIEW = 0.1;
   static readonly X_POSITION_ADD = 0;
   static readonly Y_POSITION_ADD = 12;
