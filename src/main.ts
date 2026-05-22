b23f97851effe335bee30dcd3c1db3995dd27fe6
Daniel Valencia <becse.ca@gmail.com>
2026-05-21 21:58:25 -0600

    change initialization order in main file

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { UIWebConsole } from "@ui/web-console";
 import { UIObjectContextMenu } from "./ui/object-context-menu";
 import { UI_CONSOLE_TAG_NAME, UI_OBJECT_CONTEXT_MENU_TAG_NAME } from "./shared/constants";
 
-const sandbox = new Sandbox();
-await sandbox.init();
 // Define UI custom elements
 customElements.define(UI_CONSOLE_TAG_NAME, UIWebConsole);
 customElements.define(UI_OBJECT_CONTEXT_MENU_TAG_NAME, UIObjectContextMenu);
+// Initialize world
+const sandbox = new Sandbox();
+await sandbox.init();
